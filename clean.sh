1c15dc05841c776df3847e6fd452cff4b0e00e79
Jay Lin <anton850117food1@gmail.com>
2022-05-07 12:17:36 +0800

    update: cmake

--- clean.sh
+++ clean.sh
@@ -2,3 +2,5 @@
 
 rm -rf build
 rm -rf Testing
+rm -rf html
+rm -rf latex
--- make.sh
+++ make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j 4
 ctest --test-dir build -j 4
